2c96eb97c838b36ad22b1fe7b9ef66ea84fec150
SAIKUMAR124 <pavankumar.s12459@gmail.com>
2021-11-09 09:19:28 +0530

    Added IceCreams

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import CakeContainer from "./pro1/components/CakeContainer";
 import { Provider } from "react-redux";
 import store from "./pro1/redux1/store";
 import HooksCakeContainer from "./pro1/components/HooksCakeContainer";
+import IceCreamContainer from "./pro1/components/IceCreamContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div style={{ textAlign: "center" }}>
-        <HooksCakeContainer/>
+        {/* <HooksCakeContainer/> */}
+        <IceCreamContainer/>
         <CakeContainer />
       </div>
     </Provider>
