44257a0da4ebbf3f7192eaed6ad2ce5da8fe6984
SAIKUMAR124 <pavankumar.s12459@gmail.com>
2021-11-09 10:13:52 +0530

    Added conditional removel of ice creams

--- src/pro1/redux1/iceCream/iceCreamReducer.js
+++ src/pro1/redux1/iceCream/iceCreamReducer.js
@@ -1,29 +1,41 @@
-import { ADD_ICE_CREAM, BUY_ICE_CREAM, RESET_ICE_CREAMS } from "./iceCreamTypes"
+import {
+  ADD_ICE_CREAM,
+  BUY_ICE_CREAM,
+  RESET_ICE_CREAMS,
+} from "./iceCreamTypes";
 
 const initialState = {
-    numOfIceCreams : 15
-}
+  numOfIceCreams: 15,
+};
 
-const iceCreamReducer = (state=initialState, action) => {
-    switch(action.type){
-        case BUY_ICE_CREAM:
-            return {
-                ...state,
-                numOfIceCreams: state.numOfIceCreams - 1
-            }
-        case ADD_ICE_CREAM:
-            return {
-                ...state,
-                numOfIceCreams: state.numOfIceCreams + 1
-            }
-        case RESET_ICE_CREAMS:
-            return {
-                ...state,
-                numOfIceCreams: initialState.numOfIceCreams
-            }
+const iceCreamReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_ICE_CREAM:
+      if (state.numOfIceCreams > 0) {
+        return {
+          ...state,
+          numOfIceCreams: state.numOfIceCreams - 1,
+        };
+      } else {
+        return {
+          ...state,
+          numOfIceCreams: 0,
+        };
+      }
+    case ADD_ICE_CREAM:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams + 1,
+      };
+    case RESET_ICE_CREAMS:
+      return {
+        ...state,
+        numOfIceCreams: initialState.numOfIceCreams,
+      };
 
-        default: return state
-    }
-}
+    default:
+      return state;
+  }
+};
 
 export default iceCreamReducer;
