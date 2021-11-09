e9ea2b08e943b2f7bdde54b946c0eacd98e4bf12
SAIKUMAR124 <pavankumar.s12459@gmail.com>
2021-11-09 10:58:02 +0530

    Added an Input functionality to buy cakes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Provider } from "react-redux";
 import store from "./pro1/redux1/store";
 import HooksCakeContainer from "./pro1/components/HooksCakeContainer";
 import IceCreamContainer from "./pro1/components/IceCreamContainer";
+import NewCakeContainer from "./pro1/components/NewCakeContainer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         {/* <HooksCakeContainer/> */}
         <IceCreamContainer/>
         <CakeContainer />
+        <NewCakeContainer/>
       </div>
     </Provider>
   );
