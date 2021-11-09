03ca51f54c3c8993ed9994998da0cea34786f0be
SAIKUMAR124 <pavankumar.s12459@gmail.com>
2021-11-09 10:42:00 +0530

    Added Extensions

--- src/pro1/redux1/store.js
+++ src/pro1/redux1/store.js
@@ -1,6 +1,8 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
 import rootReducer from "./RooReducer";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)))
 
 export default store;
