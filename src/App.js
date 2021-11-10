9c2c4970c4c370ad37c32e91bcffeb4919da19e4
SAIKUMAR124 <pavankumar.s12459@gmail.com>
2021-11-10 07:39:24 +0530

    mapStateToProps 2nd Arg

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import store from "./pro1/redux1/store";
 import HooksCakeContainer from "./pro1/components/HooksCakeContainer";
 import IceCreamContainer from "./pro1/components/IceCreamContainer";
 import NewCakeContainer from "./pro1/components/NewCakeContainer";
+import ItemContainer from "./pro1/components/ItemContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div style={{ textAlign: "center" }}>
         {/* <HooksCakeContainer/> */}
+        <ItemContainer cake />
+        <ItemContainer />
         <IceCreamContainer/>
         <CakeContainer />
         <NewCakeContainer/>
